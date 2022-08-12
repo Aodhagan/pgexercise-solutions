3dc036713510a54c5955bd0f87fd14df6cf4123f
Aodhagan <michael.m.malone@protonmail.com>
2022-08-12 13:51:37 +0200

    first commit

---
+++ 01_basic_questions_solutions.sql
@@ -0,0 +1,87 @@
+-- 1
+-- retrieve everything from the facilities table
+SELECT * FROM cd.facilities;
+
+-- 2 
+-- print out a list of all facilities and their cost to members
+SELECT name, membercost FROM cd.facilities;
+
+-- 3.
+-- produce a list of facilities that charge a fee to members
+SELECT facid, name, membercost, guestcost, initialoutlay, monthlymaintenance
+FROM cd.facilities
+WHERE membercost > 0;
+
+-- 4.
+-- produce a list of facilities that charge a fee to members and that fee is less than one fiftieth of the monthly maintenance cost.
+SELECT facid, name, membercost, monthlymaintenance
+FROM cd.facilities
+WHERE membercost < monthlymaintenance / 50 AND 
+      membercost > 0;
+
+-- 5.
+-- Produce a list of all facilities with the word "tennis" in their name.
+SELECT *
+FROM cd.facilities
+WHERE name LIKE '%Tennis%';
+
+-- 6.
+-- Retrieve the details of facilities with ID1 or ID5 without using OR
+SELECT * 
+FROM cd.facilities
+WHERE name LIKE '%2';
+
+-- solution 2
+SELECT * 
+FROM cd.facilities
+WHERE facid IN (1,5);
+
+-- 7.
+-- Create a new column cost that categorises if monthly maintenance is cheap or expensive
+SELECT name,
+CASE 
+    WHEN monthlymaintenance > 100 THEN 'expensive'
+    ELSE 'cheap'
+END AS cost
+FROM cd.facilities;
+
+-- 8.
+-- Produce a list of members who joined after the start of September 2012
+SELECT memid, surname, firstname, joindate
+FROM cd.members
+WHERE joindate >= '2012-09-01';
+
+-- 9.
+-- Produce an ordered list of the first ten surnames in the members table - no duplicates
+SELECT DISTINCT surname 
+FROM cd.members
+ORDER BY surname ASC 
+LIMIT 10;
+
+--10.
+-- Produce a combined list of all surnames and all facility names
+SELECT surname
+  FROM cd.members
+UNION 
+SELECT name
+  FROM cd.facilities;
+
+-- 11. 
+-- Obtain the signup date for the last member to join
+SELECT MAX(joindate) AS latest
+FROM cd.members;
+
+-- 12.
+-- Obtain the 1st and last name of the last members who signed up
+SELECT firstname, surname, joindate
+FROM cd.members
+ORDER BY joindate DESC LIMIT 1;
+
+--Solution 2
+SELECT firstname, surname, joindate
+    FROM cd.members
+    WHERE joindate = 
+        (SELECT MAX(joindate)
+            FROM cd.members)
+
+
---
+++ 02_joins_and_subqueries.sql
@@ -0,0 +1,32 @@
+-- 1. 
+-- Produce a list of the start times for bookings by members named 'David Farrell'
+
+SELECT starttime FROM cd.bookings
+INNER JOIN cd.members
+ON members.memid = bookings.memid
+WHERE members.firstname = 'David' AND
+      members.surname = 'Farrell';
+
+-- 2.
+-- Produce a list of start times for bookings for tennis courts for the date "2012-09-21". Return a list of start time and facility name pairings ordered by time
+SELECT starttime, name 
+FROM cd.bookings
+INNER JOIN cd.facilities
+ON bookings.facid = facilities.facid
+WHERE facilities.name LIKE 'Tennis%' AND 
+      starttime::date = '2012-09-21'
+ORDER BY starttime;
+
+-- Solution 2
+SELECT bookings.starttime AS start, facilities.name AS name 
+FROM cd.bookings
+INNER JOIN cd.facilities
+ON bookings.facid = facilities.facid
+WHERE facilities.name IN ('Tennis Court 2', 'Tennis Court 1') AND 
+      bookings.starttime >= '2012-09-21' AND 
+      bookings.starttime < '2012-09-22'
+ORDER BY bookings.starttime;
+
+-- 3
+-- Produce a list of all members who have recommended another member. Ensure there are no duplicates in the list and that the results are ordered by (surname, firstname)
+
